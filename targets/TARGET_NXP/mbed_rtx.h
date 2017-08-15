1dd2516fd25f2509e8292e07ca021358248d568b
JojoS <sn-mail@onlinehome.de>
2017-08-15 17:07:19 +0200

    fix LPC11U37H_401 target

--- targets/TARGET_NXP/mbed_rtx.h
+++ targets/TARGET_NXP/mbed_rtx.h
@@ -26,6 +26,7 @@
 #elif defined(TARGET_LPC11U24)        \
      || defined(TARGET_LPC11U35_401)  \
      || defined(TARGET_LPC11U35_501)  \
+     || defined(TARGET_LPC11U37H_401)  \
      || defined(TARGET_LPCCAPPUCCINO)
 
 #ifndef INITIAL_SP
